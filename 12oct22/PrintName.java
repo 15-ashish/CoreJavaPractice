847e42896f87137d932c7cb8ce525809c61b0b35
15-ashish <115772625+15-ashish@users.noreply.github.com>
2022-10-14 11:31:39 +0530

    Add files via upload

---
+++ 12oct22/PrintName.java
@@ -0,0 +1,13 @@
+class PrintName
+{
+	public static void main(String args[])
+	{        
+	         String a[]={"Name","Age","Course"};   
+			   for(int i=0;i<3;i++)
+			   {
+			   System.out.println(a[i] +":"+args[i]);
+		       //System.out.println("Age "+args[1]);
+			   //System.out.println("Course "+args[2]);
+	           }
+	}
+}
